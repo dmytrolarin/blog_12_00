e041e0def3bd5a591e4f4cb5b0669a1f023631a4
Dima <you@example.com>
2024-11-30 12:26:12 +0100

    Create author model and relation between the post model

--- blog/author_app/models.py
+++ blog/author_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length = 255)
+    surname = models.CharField(max_length = 255)
+    year_of_birth = models.SmallIntegerField()
+    country = models.CharField(max_length= 255)
+    biography = models.TextField()
+    
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+    
+    
+    
+
--- blog/post_app/models.py
+++ blog/post_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from author_app.models import Author
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     published = models.BooleanField(default=True)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE, null = True)
